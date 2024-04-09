fb21fe626720a9ea338c05e9514375a26a375045
Ahmed Khaled <ahmedkhaled4219@gmail.com>
2024-04-09 10:44:03 +0200

    lab 2-wishlist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+// import App from "./App.vue";
+import AppWrapper from "./components/AppWrapper.vue";
 
-createApp(App).mount('#app')
+createApp(AppWrapper).mount("#app");
